0bbc7e4dcb597cf2da9cb092eca2da4a4ed6fb5f
Adrian <ad24.ra12@gmail.com>
2024-02-23 16:52:47 -0600

    Agregado Event Binding y actualizacion de variable visible en FE

--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -15,9 +15,21 @@ export class LabsComponent {
   button_disabled = false;
   button_text = "Button Text";
   image_url = "https://w3schools.com/howto/img_avatar.png";
+  change_event_placeholder = "Enter a number";
+  counter = 0;
   profile = {
     name: "Adrian",
     age: 22,
     image_url: "https://w3schools.com/howto/img_avatar.png"
+  }
+
+  click_handler() {
+    alert("button clicked");
+  }
+
+  change_handler(event: Event) {
+    console.log("Event triggered")
+    console.log(event);
+    this.counter++;
   }
 }
