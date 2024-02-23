9afecbc4f6170e249260ad6b88bb849c1f763dca
Adrian <ad24.ra12@gmail.com>
2024-02-23 13:50:59 -0600

    Agregado Property Binding de manera simple y con referencia de Objetos

--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -11,4 +11,13 @@ import { Component } from '@angular/core';
 export class LabsComponent {
   example_text = 'example text';
   tasks = ['Task 1', 'Task 2', 'Task 3'];
+  input_value = "Input Value";
+  button_disabled = false;
+  button_text = "Button Text";
+  image_url = "https://w3schools.com/howto/img_avatar.png";
+  profile = {
+    name: "Adrian",
+    age: 22,
+    image_url: "https://w3schools.com/howto/img_avatar.png"
+  }
 }
