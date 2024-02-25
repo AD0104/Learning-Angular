600180a61fb642ab67c32cece3d8e9bcdfda75bd
Adrian <ad24.ra12@gmail.com>
2024-02-24 22:39:51 -0600

    Agregado Event Binding con keydown general y especifico para una combinacion de teclas

--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-labs',
@@ -16,6 +16,7 @@ export class LabsComponent {
   button_text = "Button Text";
   image_url = "https://w3schools.com/howto/img_avatar.png";
   change_event_placeholder = "Enter a number";
+  keydown_event_placeholder = "Enter new text";
   counter = 0;
   profile = {
     name: "Adrian",
@@ -31,5 +32,10 @@ export class LabsComponent {
     console.log("Event triggered")
     console.log(event);
     this.counter++;
+  }
+
+  keydown_handler(event: KeyboardEvent) {
+    const input_origin = event.target as HTMLInputElement;
+    console.log("Input Value: " + input_origin.value);
   }
 }
